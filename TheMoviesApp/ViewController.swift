a9e9d0bad47fc16f27b8c7ce1c735d7c1f80a932
Ahmed Moatasem <ahmed.mmoatasem@etisalat.com>
2025-07-15 13:53:52 +0300

    Initial Commit

---
+++ TheMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheMoviesApp
+//
+//  Created by Ahmed Moatasem on 15/07/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TheMoviesAppTests/TheMoviesAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesAppTests.swift
+//  TheMoviesAppTests
+//
+//  Created by Ahmed Moatasem on 15/07/2025.
+//
+
+import Testing
+@testable import TheMoviesApp
+
+struct TheMoviesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
